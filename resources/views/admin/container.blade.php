cc7817efd370bd6fdc60197959348003aa8e0578
unknown <briansalo1997@gmail.com>
2022-02-08 06:31:52 -0800

    update login page design and add icon title

--- resources/views/admin/container.blade.php
+++ resources/views/admin/container.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-
-
+	<title>Mini Social Media</title>
+    <link rel="icon" href="css_login/images/login2.jpg" type="image/x-icon" />
+    
 <!-------------for bootstrap 5----------->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,69 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Mini Social Media</title>
+    <link rel="icon" href="css_login/images/login2.jpg" type="image/x-icon" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+       
+    <link rel="stylesheet" href="{{ asset('css_login/style.css')}}">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    </head>
+    <body>
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        <div class="container mt-5">
+            <div class="row justify-content-center">
             </div>
+            <div class="row justify-content-center ">
+                <div class="col-md-12 col-lg-10">
+                    <div class="wrap d-md-flex">
+                        <div class="img" style="background-image: url('css_login/images/login2.jpg');">
+                        </div>
+                        <div class="login-wrap p-4 p-md-5">
+                            <div class="d-flex">
+                                <div class="w-100">
+                                    <h3 class="mb-4">Sign In</h3>
+                                </div>
+                                        <div class="w-100">
+                                            <p class="social-media d-flex justify-content-end">
+                                                <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
+                                                <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
+                                            </p>
+                                        </div>
+                            </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                            <!-- Session Status -->
+                            <x-auth-session-status class="mb-4 text-black" :status="session('status')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                            <!-- Validation Errors -->
+                            <x-auth-validation-errors class="mb-4 text-red-600" :errors="$errors" />
+
+
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group mb-3">
+                                    <label class="label" :value="__('Email')">Email</label>
+                                    <input id="email" class="form-control" type="email" name="email" :value="old('email')" placeholder="Email" required>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="label" for="password">Password</label>
+                                  <input id="password" class="form-control" type="password" name="password" placeholder="Password" required autocomplete="current-password">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="form-control rounded submit px-3 text-white" style="background-color: #0D6EFD ">Sign In</button>
+                                </div>
+                            </form>
+                             <p class="text-center">Not a member? <a data-toggle="tab" href="{{ route('register') }}">Sign Up</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+    </body>
+</html>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                        {{ __("Don't have an account?") }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
