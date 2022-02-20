cb45fd6903108fec5406490f2432a77af8768a0d
unknown <briansalo1997@gmail.com>
2022-02-20 22:58:54 +0800

    auto clear browser cache, to see my latest vue.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue().version();
+mix.js('resources/js/app.js', 'public/js').vue()
 .postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-]);
+]).version();
